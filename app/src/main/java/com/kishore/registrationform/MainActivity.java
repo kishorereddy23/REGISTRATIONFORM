c95a8f20b57925de35f1de0a0b15a72ff2add9f5
kishorereddy23 <70091012+kishorereddy23@users.noreply.github.com>
2020-09-03 13:12:35 +0530

    Initial commit

---
+++ app/src/main/java/com/kishore/registrationform/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kishore.registrationform;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.kishore.registrationform.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    String gender;
+    String lang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this
+                ,R.layout.activity_main);
+        binding.submitBtnId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (binding.male.isChecked()){
+                    gender="Male";
+                }else {
+                    gender="Female";
+                }
+                if (binding.java.isChecked()){
+                    lang="Java";
+                } if(binding.cLang.isChecked()){
+                    lang=lang+"\nC-Lang";
+                } if(binding.cLang.isChecked()){
+                    lang=lang+"\npython";
+                }
+
+                binding.tv.setText("name :"+binding.etName.getText().toString()+"\n"+
+                        "Number "+binding.etMobilenumber.getText().toString()+"\n"+
+                        binding.email.getText().toString()+"\n"+
+                        gender+"\n"+
+                        binding.spinnerBranch.getSelectedItem().toString()+"\n"+lang);
+            }
+        });
+    }
+}
+
